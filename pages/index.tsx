38f8550174cbe3f36004584b6126f0ca9aba0c9c
ToniGashi <tonigashi999@gmail.com>
2021-07-27 18:02:06 +0300

    basic structure

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Header from '@components/header'
+import Navigation from '@components/navigation';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main>
+        <Navigation/>
+        <Header />
+      </main>
+      
+    </>
+  )
+}
